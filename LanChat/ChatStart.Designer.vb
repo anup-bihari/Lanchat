6199ad950c8a80c72f1e33029af0a99665c967fd
Anup Bihari Gaurav <anupgaurav_619@rediffmail.com>
2019-10-12 20:51:32 +0530

    Add files via upload

---
+++ LanChat/ChatStart.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ChatStart
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ChatStart))
+        Me.lbComputers = New System.Windows.Forms.ListBox()
+        Me.btnRefresh = New System.Windows.Forms.Button()
+        Me.btnChat = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.SuspendLayout()
+        '
+        'lbComputers
+        '
+        Me.lbComputers.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lbComputers.FormattingEnabled = True
+        Me.lbComputers.IntegralHeight = False
+        Me.lbComputers.Location = New System.Drawing.Point(12, 12)
+        Me.lbComputers.Name = "lbComputers"
+        Me.lbComputers.Size = New System.Drawing.Size(139, 108)
+        Me.lbComputers.TabIndex = 0
+        '
+        'btnRefresh
+        '
+        Me.btnRefresh.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnRefresh.Location = New System.Drawing.Point(158, 97)
+        Me.btnRefresh.Name = "btnRefresh"
+        Me.btnRefresh.Size = New System.Drawing.Size(75, 23)
+        Me.btnRefresh.TabIndex = 1
+        Me.btnRefresh.Text = "Refresh"
+        Me.btnRefresh.UseVisualStyleBackColor = True
+        '
+        'btnChat
+        '
+        Me.btnChat.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnChat.Location = New System.Drawing.Point(158, 12)
+        Me.btnChat.Name = "btnChat"
+        Me.btnChat.Size = New System.Drawing.Size(75, 23)
+        Me.btnChat.TabIndex = 2
+        Me.btnChat.Text = "Chat"
+        Me.btnChat.UseVisualStyleBackColor = True
+        '
+        'BackgroundWorker1
+        '
+        '
+        'ChatStart
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(244, 132)
+        Me.Controls.Add(Me.btnChat)
+        Me.Controls.Add(Me.btnRefresh)
+        Me.Controls.Add(Me.lbComputers)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MinimumSize = New System.Drawing.Size(260, 170)
+        Me.Name = "ChatStart"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Connect To..."
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lbComputers As System.Windows.Forms.ListBox
+    Friend WithEvents btnRefresh As System.Windows.Forms.Button
+    Friend WithEvents btnChat As System.Windows.Forms.Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+End Class
